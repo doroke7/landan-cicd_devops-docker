f6753356b2042424eb097cf503160c89627a2483
LANDAN <landanhsu@bpit888.com>
2018-12-15 09:48:37 +0800

    change file folders

--- shells/nginx.sh
+++ shells/nginx.sh
@@ -1,4 +1,4 @@
-sudo docker build -t nginx-image - < Dockerfiles/nginx
+sudo docker build -t nginx-image - < nginx/Dockerfile
 sudo docker container stop nginx-container
 sudo docker container prune -f
 sudo docker run --name nginx-container -d -p 8080:80 nginx-image
