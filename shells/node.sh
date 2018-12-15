1f28c9bc221089130af4f4aaf73e89cc3c5f21d8
LANDAN <landanhsu@bpit888.com>
2018-12-15 17:38:46 +0800

    Fixed: node

--- shells/node.sh
+++ shells/node.sh
@@ -2,4 +2,5 @@ sudo docker build -t node-image - < node/Dockerfile
 # sudo docker container stop node-container
 sudo docker container prune -f
 
-sudo docker run -itd --name node-container -v /data:/home/vagrant/www node-image
+# docker 的 mount 只会在单一 container ，不会垮 image
+sudo docker run -itd --name node-container --mount type=bind,src=/home/vagrant/www,dst=/home/docker/www node-image
