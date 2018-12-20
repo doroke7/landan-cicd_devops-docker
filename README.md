aaaa460f148c6862893d302ffeb7296ecfe137b3
Landan Hsu <doroke7@gmail.com>
2018-12-20 23:33:10 +0800

    nginx copy files

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 ```
 sh shells/nginx.sh
 ```
+
+2. entrance the container
+```
+sudo docker exec -ti nginx-container bash
+```
--- nginx/index.sh
+++ nginx/index.sh
@@ -1,4 +1,4 @@
-sudo docker build -t nginx-image - < nginx/Dockerfile
-#sudo docker container stop nginx-container
+sudo docker build -t nginx-image .
+sudo docker container stop nginx-container
 sudo docker container prune -f
 sudo docker run --name nginx-container -d -p 80:80 nginx-image
